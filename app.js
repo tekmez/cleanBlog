33785e08021e063d6b528e0b206c2020c8713bc5
Tuncay <tuncayekmez@gmail.com>
2022-06-25 21:49:41 +0300

    MVC

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const postController = require('./controllers/postController');
+const pageController = require('./controllers/pageController');
 const ejs = require('ejs');
 const Post = require('./models/Post');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 
@@ -14,37 +17,22 @@ mongoose.connect('mongodb://localhost/cleanBlog-db', {
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
+
 // MIDDLEWARE
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method', { methods: ['POST', 'GET'] }));
+
 // Routes
-// get
-app.get('/', async (req, res) => {
-  const post = await Post.find();
-  res.render('index', { post });
-});
-app.get('/index', (req, res) => {
-  res.render('index');
-});
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-app.get('/add_post', (req, res) => {
-  res.render('add_post');
-});
-app.get('/post', (req, res) => {
-  res.render('post');
-});
-app.get('/post/:id', async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render('post', { post });
-});
-// post
-app.post('/post', async (req, res) => {
-  await Post.create(req.body);
-  res.redirect('/');
-});
+app.get('/', postController.getAllPosts);
+app.get('/post/:id', postController.getPost);
+app.get('/post/edit/:id', postController.getEditPost);
+app.get('/about', pageController.getAboutPage);
+app.get('/add_post', pageController.getAddPage);
+app.post('/post', postController.createPost);
+app.put('/post/:id', postController.updatePost);
+app.delete('/post/:id', postController.deletePost);
 
 app.listen(port, () => {
   console.log(`Server started ${port}`);
